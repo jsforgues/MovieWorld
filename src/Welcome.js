8b5a0605dc59843af009a68ed951c357faa791dd
jsforgues <jsforgues01@gmail.com>
2023-01-31 14:17:38 -0500

    corrections
    corrections

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,11 @@
+import React from "react";
 import { Container } from "semantic-ui-react";
 
 const Welcome = () => {
   return (
     <Container>
       <h1>Accueil</h1>
-      <p>Bienvenue sur notre guide de pays </p>
+      <p> </p>
     </Container>
   );
 };
